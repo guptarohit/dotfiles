c0fb81a870f5a6a99d2203cb0d256c0d462383eb
Rohit Gupta <rohitgtech+git@gmail.com>
2024-03-02 05:49:10 +0530

    add bat and help alias

--- zsh/.zsh.d/alias.zsh
+++ zsh/.zsh.d/alias.zsh
@@ -6,3 +6,9 @@ alias grep='grep --color=auto'
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Improved help
+alias help='tldr'
+
+# Replace cat with bat
+alias cat='bat'

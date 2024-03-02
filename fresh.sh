a49f0f087c6b02e681b3c6fb8468bfbed619f072
Rohit Gupta <rohitgtech+git@gmail.com>
2024-03-02 06:36:11 +0530

    make sure .config dir exists

--- fresh.sh
+++ fresh.sh
@@ -2,6 +2,8 @@
 
 echo "Setting up Mac..."
 
+mkdir -p "$HOME/.config"
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"

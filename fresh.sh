0a83813562eac31da576fb28ed6fd80caa860b27
Rohit Gupta <rohitgtech+git@gmail.com>
2024-03-03 04:40:55 +0530

    Install plugins

--- fresh.sh
+++ fresh.sh
@@ -30,6 +30,27 @@ if test ! $(which omz); then
 fi
 
 
+# Install zsh plugins
+
+ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
+
+## zsh-syntax-highlighting
+if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting" ] ; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+else
+  cd "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
+  git pull https://github.com/zsh-users/zsh-syntax-highlighting.git
+fi
+
+## zsh-autosuggestions
+if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-autosuggestions" ] ; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+else
+  cd "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
+  git pull https://github.com/zsh-users/zsh-autosuggestions.git
+fi
+
+
 echo "Setup configs via stow"
 cd $HOME/.dotfiles
 stow zsh

4ce871f52dd93e6550f9de939ebf1738acd1de34
Rohit Gupta <rohitgtech+git@gmail.com>
2024-03-09 05:08:41 +0530

    Add aliases

--- zsh/.zsh.d/alias.zsh
+++ zsh/.zsh.d/alias.zsh
@@ -16,3 +16,14 @@ alias cat='bat'
 
 # Open from the terminal
 alias o='open'
+
+# Create a directory and cd into it
+mcd() {
+    mkdir "${1}" && cd "${1}"
+}
+
+# neofetch: https://github.com/dylanaraps/neofetch
+command -v neofetch >/dev/null 2>&1 && alias neofetch="neofetch --source $HOME/.config/neofetch/nyancat.ascii"
+
+# Download file and save it with filename of remote file
+alias download="curl -O -L"
